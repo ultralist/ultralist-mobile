1aee4a512db1458d68b1df8a37665f5b783b8c3e
Grant Ammons <gammons@gmail.com>
2020-04-05 15:09:27 -0400

    mock up a todo list

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@
 
 import * as React from "react"
 import { View, StyleSheet } from "react-native"
-import { Appbar, Text } from "react-native-paper"
+import { Appbar, Text, List, Checkbox, Title, Chip } from "react-native-paper"
 
 import BottomBar from "./components/bottomBar"
+
+const ListLeft = () => {
+  return (
+    <View style={styles.listItemLeft}>
+      <Checkbox style={styles.checkbox} status="unchecked" />
+    </View>
+  )
+}
 
 const UltralistScreen = ({ navigation }) => (
   <React.Fragment>
@@ -13,10 +21,43 @@ const UltralistScreen = ({ navigation }) => (
       <Appbar.Content title="Ultralist" />
     </Appbar.Header>
 
-    <Text>Ultralist screen22</Text>
+    <Title style={styles.listTitle}>Work list</Title>
+
+    <View style={styles.filterChips}>
+      <Chip onClose={() => {}}>not:archived</Chip>
+    </View>
+
+    <List.Subheader>All items</List.Subheader>
+    <List.Section style={styles.listSection}>
+      <List.Item left={props => <ListLeft />} title="List item" description="List descriptionnnn" />
+      <List.Item left={props => <ListLeft />} title="List item" description="List description" />
+      <List.Item left={props => <ListLeft />} title="List item" description="List description" />
+    </List.Section>
 
     <BottomBar />
   </React.Fragment>
 )
+
+const styles = StyleSheet.create({
+  listItemLeft: {
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  listSection: {
+    backgroundColor: "white",
+    marginLeft: 10,
+    marginRight: 10
+  },
+  listTitle: {
+    paddingTop: 20,
+    fontSize: 30,
+    margin: 20,
+    textAlign: "center"
+  },
+  filterChips: {
+    flexDirection: "row",
+    justifyContent: "center"
+  }
+})
 
 export default UltralistScreen
