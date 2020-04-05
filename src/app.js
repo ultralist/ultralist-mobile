3ce88ec079372bf2e33ac82d9e1048bff2566f46
Grant Ammons <gammons@gmail.com>
2020-04-05 08:38:37 -0400

    create component for bottomBar

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@
 
 import * as React from "react"
 import { View, StyleSheet } from "react-native"
-import { Appbar, Text, Button, FAB } from "react-native-paper"
+import { Appbar, Text } from "react-native-paper"
+
+import BottomBar from "./components/bottomBar"
 
 const UltralistScreen = ({ navigation }) => (
   <React.Fragment>
@@ -13,63 +15,8 @@ const UltralistScreen = ({ navigation }) => (
 
     <Text>Ultralist screen22</Text>
 
-    <Appbar style={styles.bottom}>
-      <View style={styles.leftButtons}>
-        <Button onPress={() => console.log("Filter")}>
-          <Text style={styles.button}>Filter</Text>
-        </Button>
-        <Button onPress={() => console.log("Group")}>
-          <Text style={styles.button}>Group</Text>
-        </Button>
-      </View>
-      <View style={styles.rightButton}>
-        <Button onPress={() => console.log("Views")}>
-          <Text style={styles.button}>Views</Text>
-        </Button>
-      </View>
-      <View style={styles.fabHolder}>
-        <View style={styles.fab}>
-          <FAB icon="plus" onPress={() => console.log("fab")} />
-        </View>
-      </View>
-    </Appbar>
+    <BottomBar />
   </React.Fragment>
 )
-
-const styles = StyleSheet.create({
-  bottom: {
-    position: "absolute",
-    flex: 1,
-    flexDirection: "row",
-    left: 0,
-    right: 0,
-    bottom: 0
-  },
-  leftButtons: {
-    flex: 1,
-    flexGrow: 1,
-    flexDirection: "row"
-  },
-  rightButton: {
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    flex: 1
-  },
-  button: {
-    color: "#fff"
-  },
-  fabHolder: {
-    position: "absolute",
-    bottom: 30,
-    width: "100%"
-  },
-  fab: {
-    flex: 1,
-    alignItems: "center",
-    left: 0,
-    right: 0,
-    bottom: 0
-  }
-})
 
 export default UltralistScreen
---
+++ src/components/bottomBar.js
@@ -0,0 +1,68 @@
+// @flow
+
+import * as React from "react"
+import { Appbar, Button, Text, FAB } from "react-native-paper"
+import { View, StyleSheet } from "react-native"
+
+const BottomBar = () => {
+  return (
+    <Appbar style={styles.bottom}>
+      <View style={styles.leftButtons}>
+        <Button onPress={() => console.log("Filter")}>
+          <Text style={styles.button}>Filter</Text>
+        </Button>
+        <Button onPress={() => console.log("Group")}>
+          <Text style={styles.button}>Group</Text>
+        </Button>
+      </View>
+      <View style={styles.rightButton}>
+        <Button onPress={() => console.log("Views")}>
+          <Text style={styles.button}>Views</Text>
+        </Button>
+      </View>
+      <View style={styles.fabHolder}>
+        <View style={styles.fab}>
+          <FAB icon="plus" onPress={() => console.log("fab")} />
+        </View>
+      </View>
+    </Appbar>
+  )
+}
+
+const styles = StyleSheet.create({
+  bottom: {
+    position: "absolute",
+    flex: 1,
+    flexDirection: "row",
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
+  leftButtons: {
+    flex: 1,
+    flexGrow: 1,
+    flexDirection: "row"
+  },
+  rightButton: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    flex: 1
+  },
+  button: {
+    color: "#fff"
+  },
+  fabHolder: {
+    position: "absolute",
+    bottom: 30,
+    width: "100%"
+  },
+  fab: {
+    flex: 1,
+    alignItems: "center",
+    left: 0,
+    right: 0,
+    bottom: 0
+  }
+})
+
+export default BottomBar
